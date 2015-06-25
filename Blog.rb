c7505c9b1645bfac71e6e3b5ee54add09c12b05d
KeniaWriter <KeniaWriter@gmail.com>
2015-06-25 01:13:35 +0000

    Fixed .each iteration. Script is now error-free.

--- Blog.rb
+++ Blog.rb
@@ -16,9 +16,9 @@ class Blog
     
     def publish(all_blog_posts)
         all_blog_posts.each do |post|
-            puts post.title
-            puts post.time_stamp
-            puts post.content
+            puts "\n #{post.get_title}"
+            puts "#{post.get_time_stamp} \n"
+            puts post.get_content
         end
     end
         
@@ -46,15 +46,18 @@ class BlogPost
     
    def initialize
 
-       @time_stamp = Time.now
+       def get_time_stamp
+           @time_stamp = Time.now
+           return @time_stamp
+       end
         
        puts "Enter your blog post title:"
-       set_title = gets.chomp
-       @title = get_title
+       self.set_title = gets.chomp
+       @title = self.get_title
         
        puts "Enter your post\'s content:"
-       set_content = gets.chomp
-       @content = get_content
+       self.set_content = gets.chomp
+       @content = self.get_content
         
     end
 
@@ -65,17 +68,6 @@ my_first_post = my_blog.create_blog_post
 my_second_post = my_blog.create_blog_post
 all_blog_posts = my_blog.collect_all_posts
 
-#my_blog.publish(all_blog_posts)
-#I've commented out the above line because it gives me the following error:
-#Blog.rb:19:in `block in publish': undefined method `title' for #<BlogPost:0x007fae3b1c5b38> (NoMethodError)                                                   
-#        from Blog.rb:18:in `each'                                                                                                                             
-#        from Blog.rb:18:in `publish'                                                                                                                          
-#        from Blog.rb:67:in `<main>'    
-
-#So, when I inspect my blog posts, they both say 'nil' for title and content.
-#I'm pretty sure that's why my publish method gives me the error above.
-#....But I'm not understanding why those values are nil??? Help!
-puts my_first_post.inspect
-puts my_second_post.inspect
+my_blog.publish(all_blog_posts)
 
 
